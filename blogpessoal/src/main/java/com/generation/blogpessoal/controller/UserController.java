d6d8e63f300851e4cebcb879236edd61620f7df9
Nathan Coelho <nathancoelho.job@gmail.com>
2023-10-10 13:58:17 -0300

    Update of  class Service, security and Controller

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/UserController.java
@@ -0,0 +1,83 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.User;
+import com.generation.blogpessoal.model.UserLogin;
+import com.generation.blogpessoal.repository.UserRepository;
+import com.generation.blogpessoal.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<User>> getAll(){
+		return ResponseEntity.ok(userRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getById(@PathVariable Long id){
+		return userRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	
+	//@GetMapping("/user/{user}")
+	//public ResponseEntity<List<User>>  getByUser
+
+	
+	@PostMapping("/login")
+	public ResponseEntity<UserLogin> authenticateUser(@RequestBody Optional<UserLogin> userLogin){
+		return userService.authenticationUser(userLogin)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity<User> postUser(@RequestBody @Valid User user){
+		return userService.registerUser(user)
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<User> putUser(@Valid @RequestBody User user){
+		return userService.updateUser(user)
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/service/UserService.java
@@ -0,0 +1,96 @@
+package com.generation.blogpessoal.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.blogpessoal.model.User;
+import com.generation.blogpessoal.model.UserLogin;
+import com.generation.blogpessoal.repository.UserRepository;
+import com.generation.blogpessoal.security.JwtService;
+
+@Service
+public class UserService {
+
+	
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private JwtService jwtService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	private String cryptoPassword(String password) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(password);
+	}
+	
+	 private String generatedToken(String user) {
+		 return "Bearer" + jwtService.generateToken(user);
+	 }
+	 
+	 public Optional<User> registerUser(User user){
+		 if(userRepository.findByUser(user.getUser()).isPresent())
+			 return Optional.empty();
+		 
+		 user.setPassword(cryptoPassword(user.getPassword()));
+		 
+		 return Optional.of(userRepository.save(user));
+	 }
+	 
+	 public Optional<User> updateUser(User user) {
+		 if (userRepository.findById(user.getId()).isPresent()) {
+			 Optional<User> buscarUser = userRepository.findByUser(user.getUser());
+			 
+			 if((buscarUser.isPresent()) && (buscarUser.get().getId() != user.getId())){
+				 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "user exists!", null);
+			 }
+			 
+			 user.setPassword(cryptoPassword(user.getPassword()));
+			 
+			 return Optional.ofNullable(userRepository.save(user));
+		 }
+		 return Optional.empty();
+	 }
+	 
+	 
+	 public Optional<UserLogin> authenticationUser(Optional<UserLogin> userLogin){
+		 var credential = new UsernamePasswordAuthenticationToken(userLogin.get().getUser(), userLogin.get().getPassword());
+		 
+		 org.springframework.security.core.Authentication authentication = authenticationManager.authenticate(credential);
+		 
+		 if(authentication.isAuthenticated()) {
+			 Optional<User> user = userRepository.findByUser(userLogin.get().getUser());
+			 
+			 if(user.isPresent()) {
+				 userLogin.get().setId(user.get().getId());
+				 userLogin.get().setName(user.get().getName());
+				 userLogin.get().setImgUrl(user.get().getImgUrl());
+				 userLogin.get().setToken(generatedToken(userLogin.get().getUser()));
+				 userLogin.get().setPassword("");
+				 
+				 return userLogin;
+			 }
+		 }
+		 
+		 return Optional.empty();
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+}
